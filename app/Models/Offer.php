2a2fc7c86447ad873e5082f3f02254d14d8bc44a
Rising Star <star-atnight@outlook.com>
2022-02-10 21:39:15 +0800

    Add local scope for offer model

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Offer extends Model
 {
     use HasFactory;
+
+    public function scopeOfDeparture($query, $departure)
+    {
+        return $query->where('departure', $departure);
+    }
 }
