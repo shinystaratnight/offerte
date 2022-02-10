aa589d2bc4ab27b33462093d43664eee894bb5c0
Rising Star <star-atnight@outlook.com>
2022-02-11 00:38:05 +0800

    Fix up offer factory

--- database/factories/OfferFactory.php
+++ database/factories/OfferFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class OfferFactory extends Factory
@@ -13,11 +14,13 @@ class OfferFactory extends Factory
      */
     public function definition()
     {
+        $from = $this->faker->date;
+        $to = Carbon::parse($from)->addDays($this->faker->randomNumber(1) + 3);
         return [
             'hot' => $this->faker->boolean(),
             'destination' => $this->faker->city,
-            'from' => $this->faker->date,
-            'to' => $this->faker->date,
+            'from' => $from,
+            'to' => $to,
             'price' => $this->faker->randomNumber(2),
             'departure' => $this->faker->randomElement(['palermo', 'catalina', 'roma', 'milano']),
             'link' => 'https://viagginvidia.it/go/'.$this->faker->word,
